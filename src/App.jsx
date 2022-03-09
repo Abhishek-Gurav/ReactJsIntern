64c1d16826e75e386cfaf81917608e45cb56d288
Sarfraz-droid <Sarf.alam0206>
2022-03-09 12:56:51 +0530

    Working Table

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import "bootstrap/dist/css/bootstrap.min.css";
+import 'react-bootstrap-table-next/dist/react-bootstrap-table2.min.css';
 import "./App.css";
 import Homepage from "./Homepage";
 import Add from "./Add&EditNote/Add/Add";
